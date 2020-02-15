9b7034a7f77429de616c23a01c7c0cecc2a6aee4
Jacek <jaceksawko95@gmail.com>
2020-02-15 22:27:56 +0100

    added controller stubs

---
+++ src/main/java/tt/psc/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,37 @@
+package tt.psc.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tt.psc.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@RequestParam("beerId") UUID beerId) {
+        //to do impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        //to do impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@RequestParam("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        //to do impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    public ResponseEntity deleteBeerById(@RequestParam("beerId")UUID beerId){
+        //to do impl
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
